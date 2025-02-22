e5f9936cce17d7fcd1cd7e8ecd628c15b4901f99
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2025-02-22 13:27:42 +0400

    start

---
+++ src/server/api.ts
@@ -0,0 +1,17 @@
+import { remultNextApp } from "remult/remult-next";
+import { createPostgresDataProvider } from "remult/postgres";
+import { Task } from "../demo/todo/Task";
+import { getUserFromRequest } from "./auth";
+import { User } from "../demo/auth/User";
+  
+export const api = remultNextApp({
+  getUser: getUserFromRequest,
+  initApi: async () => {
+    await User.createDemoUsers();
+  },
+  dataProvider: createPostgresDataProvider({
+    connectionString: process.env["DATABASE_URL"]    
+  }),
+  admin: true,
+  entities: [Task, User],
+});
